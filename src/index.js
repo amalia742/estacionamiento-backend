436f899076e2065c5cb35df59b4addb31eafea33
amalia742 <amaliapablo801@gmail.com>
2025-07-01 21:06:48 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,30 +1,30 @@
-const express = require('express');
-const cors = require('cors');
-const dotenv = require('dotenv');
+import express from 'express';
+import dotenv from 'dotenv';
+import sequelize from './db.js';
+import vehiculoRoutes from './routes/vehiculos.routes.js';
+import cors from 'cors';
 
-// Cargar variables de entorno
 dotenv.config();
 
-// Inicializar Express
 const app = express();
-app.use(cors());
-app.use(express.json());
-
-// Importar rutas
-const entradaRoute = require('./routes/entrada.route');
-const salidaRoute = require('./routes/salida.route');
-const corteCajaRoute = require('./routes/corteCaja.route');
-
-// Usar rutas
-app.use('/entrada', entradaRoute);
-app.use('/salida', salidaRoute);
-app.use('/corte', corteCajaRoute);
-
-// Puerto desde archivo .env o 3000 por defecto
 const PORT = process.env.PORT || 3000;
 
-// Iniciar servidor
-app.listen(PORT, () => {
-  console.log(`✅ Servidor corriendo en el puerto ${PORT}`);
-});
+app.use(cors());
+app.use(express.json());
+app.use(vehiculoRoutes);
+
+(async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('📦 Conectado correctamente a la base de datos PostgreSQL');
+    await sequelize.sync(); // crea tabla si no existe
+
+    app.listen(PORT, () => {
+      console.log(`🚀 Servidor corriendo en puerto ${PORT}`);
+    });
+  } catch (error) {
+    console.error('❌ Error de conexión con la base de datos:', error);
+    process.exit(1);
+  }
+})();
 
